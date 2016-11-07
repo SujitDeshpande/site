921c2306313860b36494790aecf899283fc43c26
SujitDeshpande <sujit.md@gmail.com>
2016-11-07 05:15:49 -0700

    Added Calendar

--- resources/views/site/calendar/calendar.blade.php
+++ resources/views/site/calendar/calendar.blade.php
@@ -104,6 +104,22 @@
 </div>
 
 <!-- Mainly scripts -->
+{!! Html::script('js/plugins/fullcalendar/moment.min.js') !!}
+{!! Html::script('js/jquery-2.1.1.js') !!}
+{!! Html::script('js/bootstrap.min.js') !!}
+{!! Html::script('js/plugins/metisMenu/jquery.metisMenu.js') !!}
+{!! Html::script('js/plugins/slimscroll/jquery.slimscroll.min.js') !!}
+
+<!-- Custom and plugin javascript -->
+{!! Html::script('js/inspinia.js') !!}
+{!! Html::script('js/plugins/pace/pace.min.js') !!}
+<!-- jQuery UI custom -->
+{!! Html::script('js/jquery-ui.custom.min.js') !!}
+<!-- iCheck -->
+{!! Html::script('js/plugins/iCheck/icheck.min.js') !!}
+<!-- Full Calendar -->
+{!! Html::script('js/plugins/fullcalendar/fullcalendar.min.js') !!}
+
 <script>
 
     $(document).ready(function() {
