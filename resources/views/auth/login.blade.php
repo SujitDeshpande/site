4836d7257163de66c2244bb8ed4027c5389a2a49
SujitDeshpande <sujit.md@gmail.com>
2016-11-07 03:38:00 -0700

    Fixed the Login Flow

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,7 +13,7 @@
 
             </div>
             <h3>Welcome to IN+</h3>
-            <p>Perfectly designed and precisely prepared admin theme with over 50 pages with extra new web app views.
+            <p>Automation App
                 <!--Continually expanded and constantly improved Inspinia Admin Them (IN+)-->
             </p>
             <p>Login in. To see it in action.</p>
@@ -37,7 +37,7 @@
                 </div>
                 <button type="submit" class="btn btn-primary block full-width m-b">Login</button>
 
-                <a href="{{ url('/password/reset') }}"><small>Forgot password?</small></a>
+                <a href="/password/reset"><small>Forgot password?</small></a>
                 <p class="text-muted text-center"><small>Do not have an account?</small></p>
                 <a class="btn btn-sm btn-white btn-block" href="/register">Create an account</a>
             </form>
