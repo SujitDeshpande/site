14f33ac26c329b5bb28a7239c68ada7b2e374237
SujitDeshpande <sujit.md@gmail.com>
2016-11-08 02:38:51 -0700

    Modified Users Page

--- resources/views/site/user/user.blade.php
+++ resources/views/site/user/user.blade.php
@@ -31,22 +31,26 @@
 	            <div class="ibox-content">
 	            	<div class="table-responsive">
 						<table class="table table-striped table-bordered table-hover dataTables-example">
-							<tr>
-								<td>Id</td>
-								<td>Name</td>
-								<td>Email</td>
-								<td></td>
-							</tr>	
-							@foreach($users as $user)
-							<tr>
-								<td> {{$user->id}} </td>
-								<td><a href="#">{{ $user->name }}</a></td>
-								<td><a href="#">{{ $user->email }}</a></td>
-								<td>
-									<a data-user="{{ $user->id }}" id="user{{ $user->id }}" class="delete-user btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
-								</td>
-							</tr>
-							@endforeach
+							<thead>
+								<tr>
+									<td>Id</td>
+									<td>Name</td>
+									<td>Email</td>
+									<td>Edit/Delete</td>
+								</tr>
+							</thead>
+							<tbody>	
+								@foreach($users as $user)
+								<tr>
+									<td> {{$user->id}} </td>
+									<td><a href="#">{{ $user->name }}</a></td>
+									<td><a href="#">{{ $user->email }}</a></td>
+									<td>
+										<a data-user="{{ $user->id }}" id="user{{ $user->id }}" class="delete-user btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+									</td>
+								</tr>
+								@endforeach
+							</tbody>
 						</table>
 					</div>
 	            </div>         	
