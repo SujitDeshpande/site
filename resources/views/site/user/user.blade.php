9a777a15233404efe84c461b480fcdf4fab550d3
SujitDeshpande <sujit.md@gmail.com>
2016-11-08 02:52:32 -0700

    Modified Users Page

--- resources/views/site/user/user.blade.php
+++ resources/views/site/user/user.blade.php
@@ -8,13 +8,7 @@
 
     <title>Automation | Users</title>
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
-
-    <link href="css/plugins/dataTables/datatables.min.css" rel="stylesheet">
-
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    @include('site.includes.styles')
 
 </head>
 
@@ -82,18 +76,7 @@
 @include('site.includes.footer')
 </div>
 </div>
-    <!-- Mainly scripts -->
-    <script src="js/jquery-2.1.1.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
-    <script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-    <script src="js/plugins/jeditable/jquery.jeditable.js"></script>
-
-    <script src="js/plugins/dataTables/datatables.min.js"></script>
-
-    <!-- Custom and plugin javascript -->
-    <script src="js/inspinia.js"></script>
-    <script src="js/plugins/pace/pace.min.js"></script>
+	@include('site.includes.scripts')
 
     <!-- Page-Level Scripts -->
     <script>
