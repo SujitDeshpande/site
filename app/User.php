46312f98fa12b6c2754688de2408dea533a0456d
SujitDeshpande <sujit.md@gmail.com>
2016-11-08 01:23:19 -0700

    Added Users Page

--- app/User.php
+++ app/User.php
@@ -8,6 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
 
     /**
      * The attributes that are mass assignable.
