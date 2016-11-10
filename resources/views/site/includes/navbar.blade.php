95260cf9b2e4e1428a8ecaffa100a236849d9e8e
SujitDeshpande <sujit.md@gmail.com>
2016-11-10 05:19:23 -0700

    FirstCommit

--- resources/views/site/includes/navbar.blade.php
+++ resources/views/site/includes/navbar.blade.php
@@ -5,7 +5,7 @@
                     <div class="dropdown profile-element">
                             <a data-toggle="dropdown" class="dropdown-toggle" href="#">
                             <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">{{ Auth::user()->name }}</strong>
-                             </span> <span class="text-muted text-xs block">{{ Auth::user()->groupname }} <b class="caret"></b></span> </span> </a>
+                             </span> <span class="text-muted text-xs block">Ness Architect <b class="caret"></b></span> </span> </a>
                             <ul class="dropdown-menu animated fadeInRight m-t-xs">
                                 <li>
                                 <a href="{{ url('/logout') }}"
