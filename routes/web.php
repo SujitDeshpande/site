a76ce061fe12144187648e86e8520aac69ca574c
SujitDeshpande <sujit.md@gmail.com>
2016-11-11 02:25:49 -0700

    Profile Page

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,5 @@ Route::get('/create', 'UserAdminController@create');
 Route::post('/user', 'UserAdminController@store');
 Route::get('/user', 'UserAdminController@index');
 Route::delete('/user/{id}', 'UserAdminController@destroy');
-Route::get('/user/{id}', 'UserAdminController@profile');
+Route::post('/user/{id}', 'UserAdminController@profile');
 //Route::resource('user', 'UserAdminController');
