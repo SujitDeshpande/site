b39a794db0766a5ce4364f1ec3e0f951e0c368fb
SujitDeshpande <sujit.md@gmail.com>
2016-11-11 04:35:19 -0700

    Profile Page

--- resources/views/site/user/index.blade.php
+++ resources/views/site/user/index.blade.php
@@ -83,7 +83,7 @@
                                                                         <input name="name" value class="form-control">
                                                                     </div>
                                                                 </div>
-                                                                <p>. </p>
+                                                                <div><label class="col-sm-2 control-label"> </label></div>
                                                                 <div class="form-group">
                                                                     <label class="col-sm-2 control-label">Email</label>
                                                                     <div class="col-sm-10">
