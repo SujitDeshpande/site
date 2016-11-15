bf6deab62d949ab4cfb1ac873a90d8d18f546bf9
SujitDeshpande <sujit.md@gmail.com>
2016-11-15 04:45:05 -0700

    Added Profile Page

--- resources/views/site/user/profile.blade.php
+++ resources/views/site/user/profile.blade.php
@@ -53,7 +53,7 @@
                     <form method="get" class="form-horizontal" autocomplete="off">
 
 
-                        <img src="/uploads/avatars/default.jpg" alt="image" class="img-circle" vertical-align:middle>
+                        <img src="/uploads/avatars/default.jpg" alt="image" class="img-circle" width="40%" height="40%">
                         
                         <div class="hr-line-dashed"></div>
 
