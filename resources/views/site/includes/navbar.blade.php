e372bdd634de9670f8c293a77b1a5abb73392b1c
SujitDeshpande <sujit.md@gmail.com>
2016-11-15 02:34:50 -0700

    Added Profile Page

--- resources/views/site/includes/navbar.blade.php
+++ resources/views/site/includes/navbar.blade.php
@@ -18,7 +18,7 @@
                              </span> </a>
                             <ul class="dropdown-menu animated fadeInRight m-t-xs">
                                 <li>
-                                <a href="{{ url('/user/'+Auth::user()->id) }}">
+                                <a href="{{ url('/user/Auth::user()->id') }}">
                                     Profile
                                 </a>
                                 </li>
