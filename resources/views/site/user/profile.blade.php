304cf5ace6f29993e8f16366d6f9caee56c28b48
SujitDeshpande <sujit.md@gmail.com>
2016-11-15 02:29:20 -0700

    Added Profile Page

--- resources/views/site/user/profile.blade.php
+++ resources/views/site/user/profile.blade.php
@@ -6,26 +6,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Automation | Profile</title>
+    <title>Automation | Dashboard</title>
 
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
-
-    <link href="css/plugins/iCheck/custom.css" rel="stylesheet">
-
-    <link href="css/plugins/fullcalendar/fullcalendar.css" rel="stylesheet">
-    <link href="css/plugins/fullcalendar/fullcalendar.print.css" rel='stylesheet' media='print'>
-
-    <link href="css/plugins/dataTables/datatables.min.css" rel="stylesheet">
-
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-
-    <link href="css/plugins/chosen/chosen.css" rel="stylesheet">
-
-    <link href="css/plugins/sweetalert/sweetalert.css" rel="stylesheet">
-    
-    <meta name="csrf-token" content="{!! csrf_token() !!}"/>
+    @include('site.includes.styles')
 
 </head>
 
@@ -33,131 +16,30 @@
 
 <div id="wrapper">
 
-@include('site.includes.navbar')
+    @include('site.includes.navbar')
 
-<div id="page-wrapper" class="gray-bg">
-@include('site.includes.header')
-<div class="row wrapper border-bottom white-bg page-heading">
-    <div class="col-lg-8">
-        <h2>Automation</h2>
-        <ol class="breadcrumb">
-            <li>
-                <a href="/home">Home</a>
-            </li>
-            <li>
-                <a href="user">Users</a>
-            </li>
-            <li class="active">
-                <strong>Profile</strong>
-            </li>
-        </ol>
-    </div>
-</div>
-<div class="wrapper wrapper-content animated fadeInRight">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="ibox">
-                <div class="ibox-title">
-                    <h5>Profile</h5>
-                    <div class="ibox-tools">
-
+    <div id="page-wrapper" class="gray-bg">
+        @include('site.includes.header')
+         <div class="wrapper wrapper-content animated fadeInRight">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="text-center m-t-lg">
+                        <h1>
+                            Welcome to Automation App
+                        </h1>
+                        <small>
+                            Your controls for Automation will appear here...
+                        </small>
                     </div>
-                </div>  
-                <div class="ibox-content">
-
-                    <form method="get" class="form-horizontal" autocomplete="off">
-                         
-                        <div class="form-group"><label class="col-sm-2 control-label">Name</label>
-                            <div class="col-sm-10">
-                                <input name="name" value class="form-control">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label class="col-sm-2 control-label">Email</label>
-                            <div class="col-sm-10">
-                                <input name="email" value class="form-control" readonly>
-                            </div>
-                        </div>                                        
-
-                        <div class="hr-line-dashed"></div>
-                        <div class="form-group">
-                            <label class="col-sm-2 control-label">Password</label>
-                            <div class="col-sm-10">
-                                <input type="password" name="password" value class="form-control">
-                            </div>
-
-                        </div>
-
-                        <div class="form-group">
-                            <label class="col-sm-2 control-label">Confirm Password</label>
-                            <div class="col-sm-10">
-                                <input type="password" name="confirm_password" value class="form-control">
-                            </div>
-
-                        </div>
-
-                        <div class="hr-line-dashed"></div>
-
-
-                        <div class="form-group">
-                            <div class="col-sm-4 col-sm-offset-2">
-                                <a class="btn btn-white" href="/user"><i class="fa fa-close"></i> Cancel</a>
-                                <button class="user-create btn btn-primary" type="submit"><i class="fa fa-check"></i> Save changes</button>
-
-                            </div>
-                        </div>
-                    </form>
-                </div>       
+                </div>
             </div>
         </div>
+        @include('site.includes.footer')
+
     </div>
 </div>
 
-@include('site.includes.footer')
-</div>
-</div>
-    <!-- Mainly scripts -->
-<script src="js/plugins/fullcalendar/moment.min.js"></script>
-<script src="js/jquery-2.1.1.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
-<script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
-<script src="js/plugins/jeditable/jquery.jeditable.js"></script>
-
-<script src="js/plugins/dataTables/datatables.min.js"></script>
-
-<!-- Custom and plugin javascript -->
-<script src="js/inspinia.js"></script>
-<script src="js/plugins/pace/pace.min.js"></script>
-
-<!-- jQuery UI custom -->
-<script src="js/jquery-ui.custom.min.js"></script>
-
-<!-- iCheck -->
-<script src="js/plugins/iCheck/icheck.min.js"></script>
-
-<!-- Full Calendar -->
-<script src="js/plugins/fullcalendar/fullcalendar.min.js"></script>
-
-<script src="js/plugins/chosen/chosen.jquery.js"></script>
-
-<!-- Sweet alert -->
-<script src="js/plugins/sweetalert/sweetalert.min.js"></script>
-    
-<script type="text/javascript">
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-    });
-    $(".chosen").chosen({
-        width:'50%'
-    });
-
-</script>
-
-
+@include('site.includes.scripts')
 </body>
 
 </html>
