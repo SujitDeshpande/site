adee7911a60b18ef3b0961d9e3255c08e9712e08
SujitDeshpande <sujit.md@gmail.com>
2016-11-15 02:31:00 -0700

    Added Profile Page

--- resources/views/site/includes/navbar.blade.php
+++ resources/views/site/includes/navbar.blade.php
@@ -18,7 +18,7 @@
                              </span> </a>
                             <ul class="dropdown-menu animated fadeInRight m-t-xs">
                                 <li>
-                                <a href="/user/1">
+                                <a href="/profile">
                                     Profile
                                 </a>
                                 </li>
