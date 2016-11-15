18e01fffc3659004d0992a650457a0ebd90ad83b
SujitDeshpande <sujit.md@gmail.com>
2016-11-15 03:00:11 -0700

    Added Profile Page

--- resources/views/site/includes/styles.blade.php
+++ resources/views/site/includes/styles.blade.php
@@ -1,16 +1,10 @@
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
-
-    <link href="css/plugins/iCheck/custom.css" rel="stylesheet">
-
-    <link href="css/plugins/fullcalendar/fullcalendar.css" rel="stylesheet">
-    <link href="css/plugins/fullcalendar/fullcalendar.print.css" rel='stylesheet' media='print'>
-
-    <link href="css/plugins/dataTables/datatables.min.css" rel="stylesheet">
-
-    <link href="css/animate.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
-
-    <link href="css/plugins/chosen/chosen.css" rel="stylesheet">
-
-    <link href="css/plugins/sweetalert/sweetalert.css" rel="stylesheet">
+<link href="{{ asset('css/bootstrap.min.css') }}" rel='stylesheet' />
+<link href="{{ asset('font-awesome/css/font-awesome.css') }}" rel='stylesheet' />
+<link href="{{ asset('css/plugins/iCheck/custom.css') }}" rel='stylesheet' />
+<link href="{{ asset('css/plugins/fullcalendar/fullcalendar.css') }}" rel='stylesheet' />
+<link href="{{ asset('css/plugins/fullcalendar/fullcalendar.print.css') }}" rel='stylesheet' media='print'/>
+<link href="{{ asset('css/plugins/dataTables/datatables.min.css') }}" rel='stylesheet' />
+<link href="{{ asset('css/animate.css') }}" rel='stylesheet' />
+<link href="{{ asset('css/style.css') }}" rel='stylesheet' />
+<link href="{{ asset('css/plugins/chosen/chosen.css') }}" rel='stylesheet' />
+<link href="{{ asset('css/plugins/sweetalert/sweetalert.css') }}" rel='stylesheet' />
