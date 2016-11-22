a777f3093148aba9efb7bda088c8db19de1cff8e
SujitDeshpande <sujit.md@gmail.com>
2016-11-22 03:29:05 -0700

    Modified Migration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,13 @@
     <!-- Scripts -->
     <script src="{{ asset('/js/app.js') }}"> </script>
 
-@include('site.includes.scripts')
+<!-- Mainly scripts -->
+<script src="{{ asset('js/bootstrap.min.js') }}"> </script>
+<script src="{{ asset('js/plugins/metisMenu/jquery.metisMenu.js') }}"> </script>
+<script src="{{ asset('js/plugins/slimscroll/jquery.slimscroll.min.js') }}"> </script>
+<!-- Custom and plugin javascript -->
+<script src="{{ asset('js/inspinia.js') }}"> </script>
+<script src="{{ asset('js/plugins/pace/pace.min.js') }}"> </script>
 @yield('js')
 </body>
 </html>
