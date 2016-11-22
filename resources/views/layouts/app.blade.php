cbf48f93d85d2b646ae014d4f12e6133d6158083
SujitDeshpande <sujit.md@gmail.com>
2016-11-22 03:21:00 -0700

    Modified Migration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,11 +18,11 @@
     </script>
     <link href="{{ asset('css/bootstrap.min.css') }}" rel='stylesheet' />
     <link href="{{ asset('font-awesome/css/font-awesome.css') }}" rel='stylesheet' />
-    <link href="{{ asset('css/animate.css') }}" rel='stylesheet' />
-    <link href="{{ asset('css/style.css') }}" rel='stylesheet' />
+    
+
     @yield('css')
 
-
+    <link href="{{ asset('css/animate.css') }}" rel='stylesheet' />
 
 
 </head>
