15ba43053553a0f36cb477c67e7993bb0ee209f2
Sujit Deshpande <SujitDeshpande@Sujits-MacBook-Pro.local>
2016-11-29 00:45:38 +0530

    Updated Status appearence

--- resources/views/site/incidents/index.blade.php
+++ resources/views/site/incidents/index.blade.php
@@ -195,7 +195,7 @@
                                     </td>
 									<td>{{ $incs->category }}</td>
 									<td>{{ $incs->stream }}</td>
-                                    <td><pre>{{ $incs->comments }}</pre></td>
+                                    <pre><td>{{ $incs->comments }}</td></pre>
 
 									<td>
 										@if($logged_user_id == $incs->user_id OR Auth::user()->group_id == 1)
