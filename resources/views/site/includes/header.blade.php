403783af6137eb84879bdff5736b9f6f995fb0d9
SujitDeshpande <sujit.md@gmail.com>
2016-11-28 03:39:52 -0700

    Added incidents page

--- resources/views/site/includes/header.blade.php
+++ resources/views/site/includes/header.blade.php
@@ -14,7 +14,7 @@
                     @endforeach                
                     <li class="dropdown">
                         <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
-                            @if (count == 0)
+                            @if ($count == 0)
                             <i class="fa fa-bell"></i>  <span class="label label-warning"></span>
                             else
                             <i class="fa fa-bell"></i>  <span class="label label-warning">{{$count}}</span>
