95604a1eb9f0947a12ae6c8422c20bf3ba1e8d8c
SujitDeshpande <sujit.md@gmail.com>
2016-11-29 03:04:49 -0700

    Modified Migration

--- resources/views/site/incidents/index.blade.php
+++ resources/views/site/incidents/index.blade.php
@@ -184,8 +184,8 @@
 								<tr>
 									<td>{{ $i }} </td>
 									<td>{{ $incs->name }} </td>
-									<td>{{ $incs->created_at }} </td>
-									<td>{{ date('j-M-y', strtotime($incs->incident_no)) }} </td>
+									<td>{{ date('j-M-y', strtotime($incs->created_at)) }} </td>
+									<td>{{ $incs->incident_no }} </td>
                                     <td>
                                     @if ( $incs->status == "Complete")
                                     <span class="label label-primary">{{ $incs->status }}</span>
