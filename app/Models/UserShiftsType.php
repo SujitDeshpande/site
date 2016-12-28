2cac30702046a44881568c6fd5db420b58948ed8
SujitDeshpande <sujit.md@gmail.com>
2016-12-28 02:58:42 -0700

    Added Shift Calendar

---
+++ app/Models/UserShiftsType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use DB;
+class UserShiftsType extends Model
+{
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'user_shifts_type';
+	
+	/**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'shift_type', 'shift_desc'
+    ];
+	
+	
+	/**
+     * Get all user shifts
+     *
+     * @var $shift
+     */
+	public static function getShiftsType() {
+		$shifts = UserShiftsType::get();
+		return $shifts;
+	
+	}
+}
+
